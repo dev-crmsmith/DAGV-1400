ed4f4e36634ec62d71e07b698804d44eb413fea1
Curtis Smith <dev.crmsmith@gmail.com>
2025-02-03 17:39:59 -0700

    Scripting 4 Lab
    - Created a Lab scene based on the requirements of the scripting 4 lab.

---
+++ Scripting 4/Assets/Scripts/SimpleCharacterController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class SimpleCharacterController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 8f;
+    public float gravity = -9.91f;
+
+    private CharacterController _controller;
+    private Vector3 _velocity = Vector3.zero;
+    private Transform _thisTransform;
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _thisTransform = transform;
+    }
+
+    void Update()
+    {
+        MoveCharacter();
+        ApplyGravity();
+        KeepCharacterOn2DAxis();
+    }
+
+    private void MoveCharacter()
+    {
+        var moveInput = Input.GetAxis("Horizontal");
+        var move = new Vector3(moveInput, 0f, 0f) * (moveSpeed * Time.deltaTime);
+        _controller.Move(move);
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            _velocity.y = Mathf.Sqrt(jumpForce * -2 * gravity);
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        if (!_controller.isGrounded)
+        {
+            _velocity.y += gravity * Time.deltaTime;
+        }
+        else
+        {
+            _velocity.y = 0f;
+        }
+
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    private void KeepCharacterOn2DAxis()
+    {
+        // Prevent the character from moving on the z axis
+        var currentPosition = _thisTransform.position;
+        currentPosition.z = 0f;
+        _thisTransform.position = currentPosition;
+    }
+}
